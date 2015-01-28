a268559bf8f8582c5d04d5bde8a1e5d34568a52a
root <root@salt.hdtr.com>
2015-01-28 12:15:27 +0800

    update cmk agent plugin for xenserver

---
+++ roles/cmk/agent/files/plugins/60/xenserver_info.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# a check_mk-agent plugin,  print current host's object info
+# TODO: bond-list message-list network-list
+# time test:
+#real    0m18.744s
+#user    0m0.000s
+#sys     0m0.060s
+
+is_xenserver ()
+{
+  if [ ! -f /opt/xensource/bin/xe ];then
+    exit
+  fi
+}
+
+name_label ()
+# print current host's name label
+{
+  xl info | awk '/^host/ { print $NF }'
+}
+
+uuid ()
+# print current host's uuid
+{
+  name_label=$(name_label)
+  xe host-list name-label=$name_label --minimal
+}
+
+host_info ()
+{
+  echo "[host-info]"
+  xl info | grep -v '^$'
+}
+
+host_param_list ()
+{
+  echo "[host-param-list]"
+  uuid=$(uuid)
+  xe host-param-list uuid=$uuid
+}
+
+host_cpu_list ()
+{
+  echo "[host-cpu-list]"
+  xe host-cpu-list | grep -v '^$'
+}
+
+host_cpu_param_list ()
+{
+  cpu_list=$(xe host-cpu-list --minimal host-uuid=$(uuid) | tr ',' ' ')
+  echo "[host-cpu-param-list]"
+  for cpu in $cpu_list
+  do
+    xe host-cpu-param-list uuid=$cpu | grep -v '^$'
+  done
+}
+
+host_license_view ()
+{
+  echo "[host-license-view]"
+  xe host-license-view
+}
+
+pbd_list ()
+{
+  echo "[pbd-list]"
+  uuid=$(uuid)
+  xe pbd-list host-uuid=$uuid
+}
+
+pbd_param_list ()
+{
+  echo "[pbd-param-list]"
+  host_uuid=$(uuid)
+  pbd_list=$(xe pbd-list host-uuid=$host_uuid --minimal | tr ',' ' ')
+  for pbd in $pbd_list
+  do
+    xe pbd-param-list uuid=$pbd
+  done
+}
+
+pif_list ()
+{
+  echo "[pif-list]"
+  uuid=$(uuid)
+  xe pif-list host-uuid=$uuid
+}
+
+pif_param_list ()
+{
+  echo "[pif-param-list]"
+  host_uuid=$(uuid)
+  pif_list=$(xe pif-list host-uuid=$host_uuid --minimal | tr ',' ' ')
+  for pif in $pif_list
+  do
+    xe pif-param-list uuid=$pif
+  done
+}
+
+vm_list ()
+{
+  echo "[vm-list]"
+  uuid=$(uuid)
+  xe vm-list resident-on=$uuid is-a-template=false is-control-domain=false
+}
+
+vm_param_list ()
+{
+  echo "[vm-param-list]"
+  host_uuid=$(uuid)
+  vm_list=$(xe vm-list resident-on=$host_uuid is-a-template=false is-control-domain=false --minimal | tr ',' ' ')
+  for vm in $vm_list
+  do
+    xe vm-param-list uuid=$vm
+  done
+}
+
+sr_list ()
+{
+  echo "[sr-list]"
+  name_label=$(name_label)
+  xe sr-list host=$name_label
+}
+
+sr_param_list ()
+{
+  echo "[sr-param-list]"
+  host_name_label=$(name_label)
+  sr_list=$(xe sr-list host=$host_name_label --minimal | tr ',' ' ')
+  for sr in $sr_list
+  do
+    xe sr-param-list uuid=$sr
+  done
+}
+
+patch_list ()
+{
+  echo "[patch-list]"
+  uuid=$(uuid)
+  xe patch-list hosts:contains=$(uuid)
+}
+
+pool_list ()
+{
+  echo "[pool-list]"
+  xe pool-list 
+}
+
+pool_param_list ()
+{
+  uuid=$(xe pool-list --minimal)
+  echo "[pool-param_list]"
+  xe pool-param-list uuid=$uuid
+}
+
+is_xenserver
+echo "<<<xenserver>>>"
+host_info
+host_param_list
+host_cpu_list
+host_cpu_param_list
+pbd_list
+pbd_param_list
+pif_list
+pif_param_list
+vm_list
+vm_param_list
+sr_list
+sr_param_list
+pool_list
+pool_param_list
+patch_list
