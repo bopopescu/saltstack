f1ed2fb6d4e61bfd57c523260f3cd2718c352fa6
root <root@salt.hdtr.com>
2015-04-13 19:15:02 +0800

    fix: set_syslog_destination.sh

--- roles/XenServer/files/set_syslog_destination.sh
+++ roles/XenServer/files/set_syslog_destination.sh
@@ -1,10 +1,19 @@
-#!/bin/bas
-uuids=$(xe host-list --minimal | tr ',' '\n')
-for uuid in $uuids
-do
-    output=$(xe host-param-get param-name=logging param-key=syslog_destination uuid=$uuid)
-    if [ $output != '192.168.11.9' ];then
-       echo "set syslog destination for $uuid"
-       xe host-param-set logging:syslog_destination=192.168.11.9 uuid=$uuid
+#!/bin/bash
+test -f /etc/xensource-inventory || exit 0
+. /etc/xensource-inventory
+
+log_server=192.168.11.9
+
+xe host-param-get param-name=logging param-key=syslog_destination uuid=$INSTALLATION_UUID > /dev/null 2>&1
+
+if [ $? -eq 1 ];then
+    xe host-param-set logging:syslog_destination=$log_server uuid=$INSTALLATION_UUID
+    echo "set syslog destination to $log_server"
+else
+    output=$(xe host-param-get param-name=logging param-key=syslog_destination uuid=$INSTALLATION_UUID)
+    if [ $output != $log_server ];then
+        echo "modify syslog destination to $log_server"
+    else
+        echo "nothing changed, syslog destination is $log_server"
     fi
-done
+fi
